6b7d4e08f95f264bd2e3aa872f11be7fb35327c1
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-08-23 18:24:00 +0600

    multi grid and tick grid done

--- src/Home/MultiGrid/MultiGrid.jsx
+++ src/Home/MultiGrid/MultiGrid.jsx
@@ -4,13 +4,57 @@ const MultiGrid = () => {
     return (
         <div className="p-3 w-full">
             <div className="relative">
-                <div className="component-design bnc">
+                <div className="component-design bgc">
                     <p className="font-medium">Multi Grid <span className='text-red-600 font-semibold text-lg'>*</span></p>
-                    
+                    <div class="grid grid-rows-3 gap-4">
+
+                        <div class="flex items-center space-x-4">
+                            <div className='flex gap-3'>
+                                <p className='mt-5'>Row 1</p>
+
+                                <div className='mb-3'>
+                                    <p>Column 1</p>
+                                    <input type="radio" id="radio1" name="row1" value="radio" />
+                                </div>
+                                <div>
+                                    <p>Column 2</p>
+                                    <input type="radio" id="checkbox1" name="row1" value="checkbox1" />
+                                </div>
+                                <div>
+                                    <p>Column 3</p>
+                                    <input type="radio" id="checkbox2" name="row1" value="checkbox2" />
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <div class="flex items-center">
+                            <p className='mr-2' >Row 2 </p>
+                            <div className='flex  space-x-[70px]'>
+                                <input type="radio" id="radio2" name="row2" value="radio" />
+                                <input type="radio" id="checkbox3" name="row2" value="checkbox3" />
+                                <input type="radio" id="checkbox4" name="row2" value="checkbox4" />
+
+                            </div>
+                        </div>
+
+
+                        <div class="flex items-center">
+                            <p className='mr-2' >Row 2 </p>
+                            <div className='flex  space-x-[70px]'>
+                                <input type="radio" id="radio2" name="row2" value="radio" />
+                                <input type="radio" id="checkbox3" name="row2" value="checkbox3" />
+                                <input type="radio" id="checkbox4" name="row2" value="checkbox4" />
+
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     );
 };
 
-export default MultiGrid;MultiGrid
+export default MultiGrid; MultiGrid
--- src/Home/TickGrid/TickGrid.jsx
+++ src/Home/TickGrid/TickGrid.jsx
@@ -4,9 +4,52 @@ const TickGrid = () => {
     return (
         <div className="p-3 w-full">
             <div className="relative">
-                <div className="component-design bnc">
+                <div className="component-design bgc">
                     <p className="font-medium">Tick Grid <span className='text-red-600 font-semibold text-lg'>*</span></p>
-                    
+                    <div class="grid grid-rows-3 gap-4">
+
+                        <div class="flex items-center space-x-4">
+                            <div className='flex gap-3'>
+                                <p className='mt-5'>Row 1</p>
+
+                                <div className='mb-3'>
+                                    <p>Column 1</p>
+                                    <input type="checkbox" id="radio1" name="row1" value="radio" />
+                                </div>
+                                <div>
+                                    <p>Column 2</p>
+                                    <input type="checkbox" id="checkbox1" name="row1" value="checkbox1" />
+                                </div>
+                                <div>
+                                    <p>Column 3</p>
+                                    <input type="checkbox" id="checkbox2" name="row1" value="checkbox2" />
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <div class="flex items-center">
+                            <p className='mr-2' >Row 2 </p>
+                            <div className='flex  space-x-[70px]'>
+                                <input type="checkbox" id="radio2" name="row2" value="radio" />
+                                <input type="checkbox" id="checkbox3" name="row2" value="checkbox3" />
+                                <input type="checkbox" id="checkbox4" name="row2" value="checkbox4" />
+
+                            </div>
+                        </div>
+
+
+                        <div class="flex items-center">
+                            <p className='mr-2' >Row 2 </p>
+                            <div className='flex  space-x-[70px]'>
+                                <input type="checkbox" id="radio2" name="row2" value="radio" />
+                                <input type="checkbox" id="checkbox3" name="row2" value="checkbox3" />
+                                <input type="checkbox" id="checkbox4" name="row2" value="checkbox4" />
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
